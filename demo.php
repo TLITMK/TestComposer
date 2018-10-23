b9ff4c78f84a93d4b2bb90ff5355cab7877b2ac2
张蓝天 <18208801176@163.com>
2018-10-23 15:17:31 +0800

    'version-1'

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/23
+ * Time: 15:13
+ */
+
+require_once './vendor/autoload.php';
+use TestComp\Comp\Tools;
+Tools::Hello();
---
+++ src/TestComp/Comp/Tools.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/23
+ * Time: 15:09
+ */
+
+namespace TestComp\Comp;
+
+
+class Tools
+{
+    public function __construct(){
+
+    }
+    public static function Hello(){
+        echo "hello";
+    }
+
+}
